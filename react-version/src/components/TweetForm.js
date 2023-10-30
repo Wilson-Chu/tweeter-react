4e09c0d1ffe70f6b9ee564e05e80fa6c7cbdff1f
Wilson Chu <wilson1.chu@gmail.com>
2023-10-29 20:39:52 -0400

    refactored for react version

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,17 @@
+import React from 'react'; //optional
+
+function TweetForm() {
+  const formPhrase = "What are you humming about?";
+
+  return (
+    <section className="newtweet">
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={(event) => { event.preventDefault(); }}>
+        <textarea className="form__textarea" name="text" placeholder={formPhrase}></textarea>
+        <input type="submit" value="Tweet" className="form__input" />
+        <span className="form__counter">140</span>
+      </form>
+    </section>
+  );
+}
+
+export default TweetForm;
